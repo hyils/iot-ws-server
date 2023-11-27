00c19c0f3178cd027484f1deb1463f7ff8691bbd
hyils <3347702+hyils@users.noreply.github.com>
2023-11-27 01:02:59 -0500

    add broadcast

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+// Broadcast to all clients
+function broadcast(message) {
+    wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
+
 wss.on('connection', function connection(ws) {
     console.log('A new client connected!');
 
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
+        // Broadcast the message to all clients
+        broadcast(message);
     });
 
     ws.send('Welcome to the WebSocket server!');
