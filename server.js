dd7ae970e4b727f34291cf8ade3ba37e24d865a4
hyils <3347702+hyils@users.noreply.github.com>
2023-11-27 01:05:47 -0500

    don't send message to sender

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-// Broadcast to all clients
-function broadcast(message) {
+// Broadcast to all clients except the sender
+function broadcast(sender, message) {
     wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
+        if (client !== sender && client.readyState === WebSocket.OPEN) {
             client.send(message);
         }
     });
@@ -15,9 +15,9 @@ wss.on('connection', function connection(ws) {
     console.log('A new client connected!');
 
     ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-        // Broadcast the message to all clients
-        broadcast(message);
+        console.log('Received: %s', message);
+        // Broadcast the message to all clients except the sender
+        broadcast(ws, message);
     });
 
     ws.send('Welcome to the WebSocket server!');
